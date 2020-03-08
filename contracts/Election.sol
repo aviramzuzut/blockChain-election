27103522ea7829b49a719d4b22961b611e8ba199
aviram zuzut <aviramzuzut@gmail.com>
2020-03-09 01:06:23 +0200

    WIP

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma  solidity >=0.4.21;
 
 contract Election {
+
+    struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+}
+
     // Constructor
     constructor () public {
-        candidate = "Bibi";
+        addCandidate("Bibi");
+        addCandidate("Gantz");
+        addCandidate("Bennet");
+    }
+    // Store candidate
+    uint public candidatesCount;
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
     }
 
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Fetch candidate
+    mapping(uint => Candidate) public candidates;
 }
