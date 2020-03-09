ec4f2b9eaab218ae0a416a3f9c22063420bf823e
aviram zuzut <aviramzuzut@gmail.com>
2020-03-09 18:05:51 +0200

    WIP

--- test/election.js
+++ test/election.js
@@ -1,5 +1,11 @@
 var Election = artifacts.require("./Election.sol")
 
 contract("Election", function(accounts){
-    
+    it("initalizes with three candidates", function(){
+        return Election.deployed().then(function(instance){
+            return instance.candidatesCount();
+        }).then(function(count){
+            assert.equal(count, 3);
+        });
+    }); 
 });
